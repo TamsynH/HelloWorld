27753016f6242d5877e0177678dbba5cf399a920
TamsynH <tamsyn.courtenay@gmail.com>
2019-11-01 13:23:45 +1300

    Changing message to be more descriptive

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user
+		// Show Hello World to user
 		System.out.println("Hello World");
 
 	}
